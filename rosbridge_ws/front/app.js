68681a4e61d06b3f754c10a7392827fd2f625cba
jerry <151437856+jifoqe@users.noreply.github.com>
2025-12-17 09:07:17 +0800

    後台

---
+++ rosbridge_ws/front/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// 讓靜態檔案可以被存取
+app.use(express.static(__dirname));
+
+// 首頁
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// 一定要用這個
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log("Server running on", PORT);
+});
